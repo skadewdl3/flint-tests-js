00e6824ca50a133a53af705d3765918107d47437
Soham Karandikar <sohamk10@gmail.com>
2025-03-11 23:32:32 +0530

    Added updated jest.config.js

--- .flint/jest.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  testEnvironment: "node",
-  testMatch: ["src/*.test.js"],
-  testPathIgnorePatterns: ["node_modules/", "dist/"],
-  verbose: true,
-  collectCoverage: true,
-};
---
+++ jest.config.js
@@ -0,0 +1,13 @@
+export default {
+  "testEnvironment": "node",
+  "testMatch": [
+    "<rootDir>/src/*.test.js"
+  ],
+  "rootDir": ".",
+  "testPathIgnorePatterns": [
+    "node_modules/",
+    "dist/"
+  ],
+  "verbose": true,
+  "collectCoverage": true
+};
