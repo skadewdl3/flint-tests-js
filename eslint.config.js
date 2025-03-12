0a19b0bd3faf46f1e3ed516d1d204dbe415c04ec
Soham Karandikar <sohamk10@gmail.com>
2025-03-12 22:00:27 +0530

    update flint.toml for testing

--- eslint.config.js
+++ eslint.config.js
@@ -1,42 +1,52 @@
-import stylisticTs from '@stylistic/eslint-plugin-ts';
-import stylistic from '@stylistic/eslint-plugin';
-import stylisticJs from '@stylistic/eslint-plugin-js';
-import stylisticJsx from '@stylistic/eslint-plugin-jsx';
+import { defineConfig } from "eslint/config"
+import stylistic from "@stylistic/eslint-plugin"
+import stylisticJs from "@stylistic/eslint-plugin-js"
+import stylisticJsx from "@stylistic/eslint-plugin-jsx"
+import stylisticTs from "@stylistic/eslint-plugin-ts"
 
-export default [{
-  plugins: {
-    "@stylistic/jsx": stylisticJsx,
-    "@stylistic/ts": stylisticTs,
-    "@stylistic": stylistic,
-    "@stylistic/js": stylisticJs,
-  },
-  rules: {
-    "@stylistic/indent": [
-      "error",
-      4,
-      {
-        "SwitchCase": 5,
-        "CallExpression": {
-      },
-        "FunctionDeclaration": {
-        "parameters": 3,
-        "body": 2,
-      },
-        "FunctionExpression": {
-      },
-      }
-    ],
-    "@stylistic/quotes": [
-      "error",
-      "double",
-      {
-        "allowTemplateLiterals": "always",
-        "avoidEscape": true,
-      }
-    ],
-    "@stylistic/semi": [
-      "error",
-      "always"
-    ],
-}
-}];
+export default defineConfig( [
+    {
+        files : [
+            "src/*"
+        ],
+        ignores: [
+            "node_modules",
+            "coverage"
+        ] 
+    },
+    {
+        plugins: {
+            "@stylistic/jsx": stylisticJsx,
+            "@stylistic/ts": stylisticTs,
+            "@stylistic" : stylistic,
+            "@stylistic/js": stylisticJs 
+        },
+        rules : {
+            "@stylistic/semi": [
+                "error",
+                "always" 
+            ],
+            "@stylistic/quotes": [
+                "error",
+                "double",
+                {
+                    allowTemplateLiterals: "always",
+                    avoidEscape : true 
+                } 
+            ],
+            "@stylistic/indent": [
+                "error",
+                4,
+                {
+                    FunctionExpression : {},
+                    CallExpression : {},
+                    FunctionDeclaration: {
+                        body : 2,
+                        parameters: 3 
+                    },
+                    SwitchCase : 5 
+                } 
+            ] 
+        } 
+    } 
+] )
